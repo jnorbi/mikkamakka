81e5f5026202af24a3c992ab84de1be4da643931
jnorbi <jnorbi@gmail.com>
2022-04-26 13:15:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// 20220426
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
